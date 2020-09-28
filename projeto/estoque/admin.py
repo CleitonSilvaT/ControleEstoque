ac16b124b97f4715fdd012db8a3101814afb43c2
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-09-28 05:59:31 -0300

    Update EstoqueAdmin. Close #12

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Estoque
+from .models import EstoqueItens
 
 # Register your models here.
+
+# admin.site.register(Estoque)
+# admin.site.register(EstoqueItens)
+
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'nf')
+    search_fields = ('nf',)
+    list_filter = ('funcionario',)
+    date_hierarchy = 'created'
