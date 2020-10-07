db5c962feee26e3a4aa3bebe8776c1a2ece386d6
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-10-07 06:26:09 -0300

    Criada função para adicionar produto. Close #20

--- projeto/produto/urls.py
+++ projeto/produto/urls.py
@@ -6,4 +6,5 @@ app_name = 'produto'
 urlpatterns = [
     path('', views.lista_produto, name='lista_produto'),
     path('<int:pk>/', views.detalhe_produto, name='detalhe_produto'),
+    path('add/', views.ProdutoCreate.as_view(), name='adiciona_produto'),
 ]
