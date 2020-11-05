35855e7c353a86fd0049d72eecc6c82206b009df
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-11-05 05:47:48 -0300

    Baixando produto no estoque de entrada. Close #26

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, resolve_url
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
+from projeto.produto.models import Produto
 from .models import Estoque, EstoqueItens
 from .forms import EstoqueForm, EstoqueItensForm
 
@@ -19,6 +20,18 @@ def estoque_entrada_detail(request, pk):
     context = {'object':obj}
 
     return render(request, template_name, context)
+
+def realizar_baixa_estoque(form):
+    # Pegar produto a partir da instância do formulário (Estoque)
+    produtos = form.estoques.all()
+
+    for item in produtos:
+        produto = Produto.objects.get(pk=item.produto.pk)
+        produto.estoque = item.saldo
+        produto.save()
+    
+    print('Estoque atuaizado com sucesso')
+
 
 def estoque_entrada_add(request):
     template_name = 'estoque_entrada_form.html'
@@ -39,6 +52,7 @@ def estoque_entrada_add(request):
         if form.is_valid() and formset.is_valid():
             form = form.save()
             formset = formset.save()
+            realizar_baixa_estoque(form)
             url = 'estoque:estoque_entrada_detail'
             return HttpResponseRedirect(resolve_url(url, form.pk))
     else:
